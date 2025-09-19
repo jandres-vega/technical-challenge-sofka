dd620036941afdbac59e93f300f414ec0347a8bd
jorge.vega <jorge.vega@sofka.com.co>
2025-09-19 11:47:24 -0500

    feat: Update configuration to use environment variables for customer person URL

--- account-movement-msv/src/main/java/com/sofka/accountMovement/infrastructure/external/CustomerClient.java
+++ account-movement-msv/src/main/java/com/sofka/accountMovement/infrastructure/external/CustomerClient.java
@@ -5,7 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "customer-person-msv", url = "http://localhost:8000")
+@FeignClient(
+        name = "customer-person-msv",
+        url = "${customer.person.url}"
+)
 public interface CustomerClient {
 
     @GetMapping("/clientes/{id}")
