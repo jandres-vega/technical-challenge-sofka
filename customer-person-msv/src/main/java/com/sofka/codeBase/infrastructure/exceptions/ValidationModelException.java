9fc3d7d7eff791606525e734a48bc92132f9dbd9
jorge.vega <jorge.vega@sofka.com.co>
2025-09-19 12:44:10 -0500

    feat: Remove ValidationModelException class as it is no longer needed

--- customer-person-msv/src/main/java/com/sofka/codeBase/infrastructure/exceptions/ValidationModelException.java
+++
@@ -1,9 +0,0 @@
-package com.sofka.codeBase.infrastructure.exceptions;
-
-import org.springframework.http.HttpStatus;
-
-public class ValidationModelException extends BaseException {
-    public ValidationModelException(String message) {
-        super(message, HttpStatus.BAD_REQUEST);
-    }
-}
