42f3fa80c1afeee3cbca13dc5da5aac768bb6469
jorge.vega <jorge.vega@sofka.com.co>
2025-09-19 12:44:28 -0500

    feat: Add ComponentScan annotation to include additional packages for component scanning

--- customer-person-msv/src/main/java/com/sofka/customerPerson/CustomerPersonMsvApplication.java
+++ customer-person-msv/src/main/java/com/sofka/customerPerson/CustomerPersonMsvApplication.java
@@ -2,8 +2,10 @@ package com.sofka.customerPerson;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.sofka.codeBase", "com.sofka.customerPerson"})
 public class CustomerPersonMsvApplication {
 
     public static void main(String[] args) {
